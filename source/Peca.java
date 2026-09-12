2d67947be14068e35ee7aaa7ff832c5b289b0e46
Mozzer <GrozzerMozzer@gmail.com>
2026-09-12 10:08:51 -0300

    feat: pecas exclusivas no Peca.java(#11)

--- source/Peca.java
+++ source/Peca.java
@@ -4,12 +4,21 @@ public class Peca {
     private String nome;
     private double preco;
     private String descricao;
+    private boolean exclusiva;
 
     public Peca(String nome, double preco, String descricao) {
         this.nome = nome;
         this.preco = preco;
         this.descricao = descricao;
+        this.exclusiva = false;
     }
+    public Peca(String nome, double preco, String descricao, boolean exclusiva){
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.exclusiva = exclusiva;
+    }
+
     public String getNome(){
         return nome;
     }
@@ -19,9 +28,14 @@ public class Peca {
     public String getDescricao() {
         return descricao;
     }
+    public boolean isExclusiva() {
+        return exclusiva;
+    }
+
     @Override
     public String toString() {
-        return String.format("%s - R$ %.2f\n %s",nome,preco,descricao);
+        String marca = exclusiva ? "Estrela " : "";
+        return String.format("%s%s - R$ %.2f\n   %s", marca, nome, preco, descricao);
     }
 }
 
