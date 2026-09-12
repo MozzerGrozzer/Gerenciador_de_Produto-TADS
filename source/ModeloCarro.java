e7a5a148746e092ac7de98f7b08bc9ff1f61e4ef
Mozzer <GrozzerMozzer@gmail.com>
2026-09-12 09:57:28 -0300

    feat: modelo carro(#9)

---
+++ source/ModeloCarro.java
@@ -0,0 +1,29 @@
+package source;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModeloCarro {
+    private String nome;
+    private java.util.map<String, Peca> pecasExclusivas;
+
+    public ModeloCarro(String nome) {
+        this.nome = nome;
+        this.pecasExclusivas = new java.util.HashMap<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public voit adicionarPecaExclusiva(String categoria, Peca peca){
+        pecasExclusivas.put(categoria, peca);
+    }
+
+    public Peca getPecaExclusiva(String categoria) {
+        return pecasExclusivas.get(categoria);
+    }
+
+    public java.util.Map<String, Peca> getPecaExclusiva(){
+        return pecasExclusivas;
+    }
+}
